fd156e0af8e261dfd24519d2827ad2521b525644
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2021-03-18 00:08:02 +0200

    swagger docs

--- src/main/java/dc/vilnius/Application.java
+++ src/main/java/dc/vilnius/Application.java
@@ -1,7 +1,18 @@
 package dc.vilnius;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Hero app",
+        version = "0.0.1",
+        description = "Hero API",
+        license = @License(name = "MIT", url = "https://github.com/urbontaitis/hero-app/blob/main/LICENSE")
+    )
+)
 public class Application {
 
     public static void main(String[] args) {
